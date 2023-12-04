bf58fbfb72dcb5a52d8441c271fc457ab5421d22
Cristian <230183@fppiramide.com>
2023-12-04 10:16:21 +0100

    Empezando casos de uso

---
+++ periodista/applicattion/Periodista.usecases.ts
@@ -0,0 +1,11 @@
+import Periodista from "../domain/Periodista";
+import PeriodistaRepository from "../domain/Periodista.repository";
+
+export class PeriodistaUseCases{
+    private periodistaRepository: PeriodistaRepository;
+
+    constructor(periodistaRepository: PeriodistaRepository){
+        this.periodistaRepository=periodistaRepository;
+    }
+    async 
+}
